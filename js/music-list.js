55c53b58ee5ffdce3ab1f37331791961009e71ab
aklavya7 <aklavyaanurag3@gmail.com>
2022-09-05 10:24:33 +0530

    MuzikPlayer

---
+++ js/music-list.js
@@ -0,0 +1,41 @@
+
+
+let allMusic = [
+  {
+    name: "Faded",
+    artist: "Alan Walker",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Dil Ka Dariya",
+    artist: "Arijit Singh",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Unstoppable",
+    artist: "Sia",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Lut Gaye",
+    artist: "Jubin Nautiyal",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Bebo",
+    artist: "Yo Yo Honey Singh",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Chup Mahi Chup Hai Ranjha",
+    artist: "B Praak",
+    img: "music-6",
+    src: "music-6"
+  },
+  
+];
